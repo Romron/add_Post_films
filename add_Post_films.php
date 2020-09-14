5c5a3a97184fddd897b55664e44905e8ae6c57dc
Romron <romron@ukr.net>
2020-09-14 21:16:47 +0300

    добавлена страница настроек, файлы: функций, стилей, скриптов

--- add_Post_films.php
+++ add_Post_films.php
@@ -1,4 +1,24 @@
 <?php
 /*
- * Plugin Name: add_Post_films
+    Plugin Name: add_Post_films
+    Description: Добавляет посты с постерами 
+
  */
+
+
+add_action("admin_menu", "admin_add_menu_add_Post_films");
+
+function admin_add_menu_add_Post_films(){ 
+    add_menu_page( 'Settings add_Post_films Plugin',  
+                    'add_Post_films', 
+                    'manage_options', 
+                    plugin_dir_path( __FILE__ ).'settings.php',  
+                    ''
+                );
+    };
+
+
+
+
+
+
