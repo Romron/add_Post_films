7130214f699e105ff8c8c66bec33efe3473402b4
Romron <romron@ukr.net>
2020-09-14 23:05:56 +0300

    вроде всё готово для начала разработки

--- functions_add_Post_films.php
+++ functions_add_Post_films.php
@@ -4,7 +4,15 @@
 function Json__addPF($file_name_addPF){
 	
 	$json_str = file_get_contents ($file_name_addPF);
-	$arr_addPF = json_decode($json_str, true);
-	print_r($arr_addPF);
+	$arr_posts_addPF = json_decode($json_str, true);
+	return $arr_posts_addPF;
 
+	}
+
+function Insert_posts_addPF($arr_posts_addPF){
+	foreach ($arr_posts_addPF as $key => $value) {
+		// $post_id = wp_insert_post($value, true );
+		echo($key . '=>' . $value);
+		}
+	
 	}
--- settings.php
+++ settings.php
@@ -6,8 +6,14 @@ require_once("style_add_Post_films.css");
 require_once("script_add_Post_films.js");
 
 
-$file_name_addPF = __DIR__. '\json\result_DateAboutAllFilms  TEST .json';
+$file_name_addPF = __DIR__. '\json\result_DateAboutAllFilms  TEST 10 .json';
 
-Json__addPF($file_name_addPF);
+$arr_posts_addPF = Json__addPF($file_name_addPF);
+
+echo('<pre>');
+print_r($arr_posts_addPF);
+echo('</pre>');
+
+Insert_posts_addPF($arr_posts_addPF);
 
 
